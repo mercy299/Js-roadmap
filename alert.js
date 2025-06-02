0481af499536e900e9691da16cdf56afaac0b56d
mercy299 <mercysam299@gmail.com>
2025-06-02 10:30:00 +0100

    first commit

--- alert.js
+++ alert.js
@@ -1,1 +1,21 @@
 alert("Hello, world!");
+
+
+let user = 'John', age = 25, message = 'Hello';
+
+
+// const NAME = 'John';
+
+// let userName = NAME;
+
+let admin, name
+
+name = 'John';
+admin = name;
+alert(admin); // John
+
+let ourPlanetName = 'Earth';
+let currentUserName = 'John Doe';
+
+let birthday = '18.04.1982';
+alert("Your bithdate is" + birthday);
