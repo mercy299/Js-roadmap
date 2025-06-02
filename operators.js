9119f42c1e26bdf6df4e0c3685e3d8d5c98c4587
mercy299 <mercysam299@gmail.com>
2025-06-02 14:34:59 +0100

    page commit

---
+++ operators.js
@@ -0,0 +1,11 @@
+// An operator is unary if it has a single operand. For example, the unary negation - reverses the sign of a number:
+let x = 1;
+
+x = -x;
+alert( x ); // -1, unary negation was applied
+
+let a = 1, b = 3;
+alert( b - a ); // 2, binary minus subtracts values
+
+// Note that if any of the operands is a string, then the other one is converted to a string too.
+
---
+++ strconversion.js
@@ -0,0 +1,8 @@
+let str = "123";
+alert(typeof str); // string
+
+let num = Number(str); // becomes a number 123
+
+alert(typeof num); // number
+
+// Please note that null and undefined behave differently here: null becomes zero while undefined becomes NaN
