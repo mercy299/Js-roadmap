05e1a627ffc106d1f90b46217cd62438446ee866
mercy299 <mercysam299@gmail.com>
2025-06-15 11:53:21 +0100

    edited loops:while-and-for.js

--- loops:while-and-for.js
+++ loops:while-and-for.js
@@ -10,3 +10,36 @@ while (i < 3) {
 }
 
 // While the condition is truthy, the code from the loop body is executed.
+
+// Curly braces are not required for a single-line body
+// If the loop body has a single statement, we can omit the curly braces {…}:
+
+let j = 3;
+while (j) alert(j--);
+
+// The “do…while” loop
+do {
+  // loop body
+} while (condition);
+
+// The loop will first execute the body, then check the condition, and, while it’s truthy, execute it again and again.
+
+let m = 0;
+do {
+  alert(m);
+  m++;
+} while (m < 3);
+
+// for loop
+
+for (begin; condition; step) {
+  // ... loop body ...
+}
+
+for (let w = 0; w < 3; w++) {
+  alert(w++);
+}
+
+for (;;) {
+  // repeats without limits
+}
