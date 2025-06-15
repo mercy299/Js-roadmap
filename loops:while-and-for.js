1a53153fa9e8447248c139d22b537c1cf12fdaff
mercy299 <mercysam299@gmail.com>
2025-06-15 09:13:17 +0100

    edited loops:while-and-for.js

---
+++ loops:while-and-for.js
@@ -0,0 +1,12 @@
+while (condition) {
+  // code
+  // so-called "loop body"
+}
+
+let i = 1;
+while (i < 3) {
+  alert(i);
+  i++;
+}
+
+// While the condition is truthy, the code from the loop body is executed.
