00916ef7c1fa05421f900016ee171cfb2752489d
mercy299 <mercysam299@gmail.com>
2025-06-15 16:46:26 +0100

    edited loops:while-and-for.js

---
+++ switch.js
@@ -0,0 +1,28 @@
+// A switch statement can replace multiple if checks.
+
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert("Too small");
+    break;
+  case 4:
+    alert("Exactly!");
+    break;
+  case 5:
+    alert("Too big");
+    break;
+  default:
+    alert("I don't know such values");
+}
+
+// If there is no break then the execution continues with the next case without any checks.
+
+// if statements evaluate boolean expressions, allowing for flexible comparisons using logical and relational operators. switch statements, on the other hand, evaluate a single expression against multiple possible case values, typically for equality comparisons.
+if (browser == "Edge") {
+  alert("You've got the edge");
+} else if (browser == "Chrome" || browser == "Firefox" || browser == "Safari") {
+  alert(" Okay we support these browsers too");
+} else {
+  alert("We hope that this page looks ok!");
+}
