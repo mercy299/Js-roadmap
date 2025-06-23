211a9198a0b631a3f6c183e179ad4b6b940a0fdd
mercy299 <mercysam299@gmail.com>
2025-06-23 10:13:33 +0100

    edited  functions.js

--- functions.js
+++ functions.js
@@ -12,3 +12,21 @@ function showMessage() {
 showMessage(); // Hello, I'm JavaScript!
 
 alert(message); // <-- Error! The variable is local to the function
+
+// no matter how the function is created, a function is a value.
+
+// A function is a value representing an “action”
+
+// Function Declaration: a function, declared as a separate statement, in the main code flow:
+
+// Function Declaration
+function sum(a, b) {
+  return a + b;
+}
+
+// Function Expression: a function, created inside an expression or inside another syntax construct. Here, the function is created on the right side of the “assignment expression” =:
+
+// Function Expression
+let sum = function (a, b) {
+  return a + b;
+};
